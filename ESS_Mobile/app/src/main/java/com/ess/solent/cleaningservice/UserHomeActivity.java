b55b7730733059ea1ac20d8dc19358a3da79463f
James Richardson <3richj71@solent.ac.uk>
2019-03-06 12:15:46 +0000

    upload of mobile app

---
+++ ESS_Mobile/app/src/main/java/com/ess/solent/cleaningservice/UserHomeActivity.java
@@ -0,0 +1,42 @@
+package com.ess.solent.cleaningservice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class UserHomeActivity extends AppCompatActivity implements View.OnClickListener {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.activity_home_user);
+        Button service = (Button)findViewById(R.id.button1);
+        service.setOnClickListener(this);
+        Button schedule = (Button)findViewById(R.id.button2);
+        schedule.setOnClickListener(this);
+        Button history = (Button)findViewById(R.id.button3);
+        history.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button1){
+            Intent intent = new Intent(this,UserServiceActivity.class);
+            startActivity(intent);
+        }
+        if(v.getId() == R.id.button2){
+            Intent intent = new Intent(this,UserScheduleActivity.class);
+            startActivity(intent);
+        }
+        if(v.getId() == R.id.button3){
+            Intent intent = new Intent(this,UserServiceHistoryActivity.class);
+            startActivity(intent);
+        }
+    }
+}
