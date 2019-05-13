2ece57652a817334e43b8194d2d1f7e67cd7a496
Aaron Jenkins <3JENKA17@solent.ac.uk>
2019-05-13 14:52:39 +0100

    login almost working

--- ESS_Desktop/src/Login.java
+++ ESS_Desktop/src/Login.java
@@ -6,6 +6,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import org.json.JSONObject;
+import org.json.JSONException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Login extends Application {
 
@@ -41,6 +47,36 @@ public class Login extends Application {
         GridPane.setConstraints(passIn, 1, 1);
 
         Button loginButton = new Button("Log in!");
+        loginButton.setOnAction(event -> {
+            HttpURLConnection conn = null;
+            try {
+                URL url =  new URL ("http://104.248.168.181/REST/login/userLogin.php");
+                conn = (HttpURLConnection) url.openConnection();
+                String username = usernameIn.getText();
+                String pw = passIn.getText();
+                String postData = "username="+username + "&password="+pw;
+                conn.setDoOutput(true);
+                conn.setFixedLengthStreamingMode(postData.length());
+                OutputStream out = null;
+                out = conn.getOutputStream();
+                out.write(postData.getBytes());
+                if (conn.getResponseCode() == 200) {
+                    System.out.println("true");
+                    InputStream in = conn.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                    String all = "", line;
+                    while((line = br.readLine()) !=null)
+                        all += line;
+                    JSONObject jsonObj = new JSONObject (json);
+                    System.out.println(all);
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
         GridPane.setConstraints(loginButton, 0, 2);
 
         grid.getChildren().addAll(userLabel, usernameIn, passLabel, passIn, loginButton);
