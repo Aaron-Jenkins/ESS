7befe8b6a9da83e490b77ffa257369fbbe7092c4
James Richardson <3richj71@solent.ac.uk>
2019-05-15 14:50:10 +0000

    some updates to the rest api

--- REST/create/createJob.php
+++ REST/create/createJob.php
@@ -8,24 +8,37 @@
 
 
 
-$serviceId  = isset($_POST['serviceId'])    ? $_POST['serviceId']   : '';
-$startDate  = isset($_POST['startDate'])    ? $_POST['startDate']   : '';
-$endDate    = isset($_POST['endDate'])      ? $_POST['endDate']     : '';
+$userId     = isset($_GET['userId'])       ? $_GET['userId']      : '';
+$serviceId  = isset($_GET['serviceId'])    ? $_GET['serviceId']   : '';
+$startDate  = isset($_GET['startDate'])    ? $_GET['startDate']   : '';
+$endDate    = isset($_GET['endDate'])      ? $_GET['endDate']     : '';
+$empId      = isset($_GET['empId'])        ? $_GET['empId']       : '';
 
 
-if(!empty($serviceId) && !empty($startDate) && !empty($endDate)){
+echo $serviceId . " - " . $userId . " - " . $startDate . " - " .$endDate . " - " . $empId;
+
+if(!empty($userId) && !empty($serviceId) && !empty($startDate) && !empty($endDate)){
     $conn = new PDO ("mysql:host=localhost;dbname=cleaning;", "cleaning", "cleaningpassword");
-    $sql = "INSERT INTO company (companyName ) VALUES (:companyName)";
+    $sql = "INSERT INTO jobs (userId, serviceId, startDate, endDate, empId ) VALUES (:userId, :serviceId, :startDate, :enddate, :empId)";
     $stmt = $conn->prepare($sql);
-    $stmt->bindValue(":companyName", $companyName);
+    $stmt->bindValue(":id", NULL);
+    $stmt->bindValue(":userId", $userId);
+    $stmt->bindValue(":serviceId", $serviceId);
+    $stmt->bindValue(":startDate", $startDate);
+    $stmt->bindValue(":endDate", $endDate);
+    $stmt->bindValue(":empId", $empId);
     $result = $stmt->execute();
+
     if($result){
         $returnData = array('status' => true, 'message' => 'Company Added Successfully...');
     }else{
         $returnData = array('status' => false, 'message' => 'Can\'t Add Company...');
+        echo $result;
     }
     header('Content-Type: text/json');
     echo json_encode($returnData);
+}else{
+    echo "Error!";
 }
 
 
